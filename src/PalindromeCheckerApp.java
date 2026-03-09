1a6fd4a171f670467122776afbecb547a1c872d1
Madlab41 <santypravin3@gmail.com>
2026-03-09 11:43:59 +0530

    UC4: Add case insensitive palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -11,16 +11,18 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a word: ");
         String word = sc.nextLine();
 
+        String original = word.toLowerCase();
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        System.out.println("Original word  : " + word);
-        System.out.println("Reversed word  : " + reversed);
+        System.out.println("Input word      : " + word);
+        System.out.println("Converted word  : " + original);
+        System.out.println("Reversed word   : " + reversed);
 
-        if (word.equals(reversed)) {
+        if (original.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is NOT a Palindrome");
